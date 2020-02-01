1ebab244b6088a4b33e07196f369c2c298e133a6
Andrew Binnie <binnieandrew@gmail.com>
2020-02-01 11:29:57 +0000

    adding the pasenger class and first test

---
+++ src/main/java/Passenger.java
@@ -0,0 +1,16 @@
+public class Passenger {
+
+    private String name;
+    private int bagQuantity;
+
+    public Passenger(String name, int bagQuantity) {
+        this.name = name;
+        this.bagQuantity = bagQuantity;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+
+}
---
+++ src/test/java/PassengerTest.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+// Calendar now = new GregorianCalendar(Timezone.getTimeZone());
+
+
+public class PassengerTest {
+
+    private Passenger passenger;
+    private String name;
+    private int bagQuantity;
+   // private Flight flight;
+    private int seatNumber;
+
+    @Before
+    public void before(){
+        passenger = new Passenger("Kate", 2);
+ //       flight = new Flight("FR100", "YVR", "GLA", "21:00");
+
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Kate", passenger.getName());
+    }
+
+}
